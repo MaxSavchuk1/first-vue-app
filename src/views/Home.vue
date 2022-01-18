930e3f9bf2da10d3441e59af192c7237ee4db664
Maxim Savchuk <m.savchuk@urancompany.com>
2022-01-18 15:30:57 +0200

    created sample from lesson

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non aperiam,
+      voluptatibus aut temporibus iusto mollitia quo nemo architecto dolorem
+      voluptate.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
